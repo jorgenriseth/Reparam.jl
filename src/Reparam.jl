d323d7d88078476f1e7a4f9efc942f9200af6a26
Jorgen Riset <jnriseth@gmail.com>
2020-10-22 14:58:00 +0200

    Mulitpple changes and added some chaos notebooks

--- src/Reparam.jl
+++ src/Reparam.jl
@@ -1,17 +1,18 @@
 module Reparam
 
-# Write your package code here.
-include("interpolation/lagrange.jl")
-export CreateLagrangeInterpolator, CreatePiecewiseLagrangeInterpolator
-
-include("interpolation/util.jl")
-export compute_error_2_norm, compute_error_max_norm, square_dist_sum
-export create_nodes_chebyshev, create_nodes_equidistant
-
-include("interpolation/gradient_descent.jl")
-export GradientDescent
-
-include("interpolation/rbf.jl")
-export CreateRbfInterpolator
+# include("functionals.jl")
+# include("plotters.jl")
+# include("transform.jl")
+# include("linesearch.jl")
+# include("projection.jl")
+# include("reparametrization.jl")
 
 end #module
+
+
+include("functionals.jl")
+include("plotters.jl")
+include("transform.jl")
+include("linesearch.jl")
+include("projection.jl")
+include("reparametrization.jl")
--- src/reparametrization.jl
+++ src/reparametrization.jl
@@ -8,3 +8,37 @@ function l2_gradient(q, r)
         return r(t)⋅qdt(t) - q(t)⋅rdt(t)  # /diff_norm
     end
 end
+
+function accumulate_composition(funcvec)
+    n = length(funcvec)
+    out = Vector{Function}(undef, n)
+    out[1] = funcvec[1]
+    for i in 2:n
+        out[i] = out[i-1] ∘ funcvec[i]
+    end
+    return out
+end
+
+
+function relative_error(f1, f0)
+    return abs((f1 - f0) / f0)
+end
+
+
+function printheader(;width=82)
+    println(" " * "-"^width)
+    @printf "| %-4s | %-16s | %-16s | %-16s | %-16s | %-16s | \n" "Iter" "Error" "εmax" "εi" "||dγ||" "|f_i - f_{i-1}|/ |fi|"
+    println("-"^width)
+end
+
+function printfirst(error)
+    @printf "| %4d | %.10e | %-16s | %-16s | %-16s |\n" 0 error "" "" ""
+end
+
+function printline(iter, error, max_step, step, gradnorm, relerror)
+    @printf "| %4d | %.10e | %.10e | %.10e | %.10e | %.10e |\n" iter error max_step step gradnorm relerror
+end
+
+function printfooter(;width=82)
+    println(" " * "-"^width)
+end
