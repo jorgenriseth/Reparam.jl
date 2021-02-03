ab377ffc52187d8e9b08a3e7259f6f3fc1a070dc
Jorgen Riset <jnriseth@gmail.com>
2021-02-03 14:48:34 +0100

    remains from previous

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,7 @@ using Reparam
 using Test
 
 @testset "All Tests" begin
-    #@testset "Interpolation" begin include("test_interpolation.jl")
-    #end
-
-    #@testset "Gradient Descent" begin
-    #    include("test_gradient_descent.jl")
-    #end
-    1 == 1 
+    @testset "Circle" begin
+        include("circle.jl")
+    end
 end
